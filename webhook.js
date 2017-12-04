796cccd1d51c914870afa489f7f58a6d7d8f5b7f
christylaguardia <christinelaguardia@gmail.com>
2017-12-03 23:58:50 -0800

    added basic express server

--- webhook.js
+++ webhook.js
@@ -1,6 +1,18 @@
+const express = require('express');
+const app = express();
 const request = require('superagent');
+const PORT = process.env.PORT || 3000;
 
-request
-  .get('https://api.chucknorris.io/jokes/random')
-  .then(res => res.body)
-  .catch(err => console.log(err.status, 'error', err));
+app.use('/', (req, res) => {
+  return request
+    .get('https://api.chucknorris.io/jokes/random')
+    .then(res => {
+      console.log('chuck norris says:', res.body.value)
+      return res.body.value;
+    })
+    .catch(err => console.log(err.status, 'error', err));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}!`);
+});
