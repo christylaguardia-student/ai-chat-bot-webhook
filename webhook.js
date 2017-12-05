28f907affdc697c2749c69efd5af2d0db4a06574
christylaguardia <christinelaguardia@gmail.com>
2017-12-05 00:48:28 -0800

    added best deal api call

--- webhook.js
+++ webhook.js
@@ -7,19 +7,28 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 const appId = process.env.APP_ID;
+let ebayApiUrl = 'http://svcs.ebay.com/services/search/FindingService/v1'
+ebayApiUrl += '?OPERATION-NAME=findItemsByKeywords'
+ebayApiUrl += '&SERVICE-VERSION=1.0.0';
+ebayApiUrl += `&SECURITY-APPNAME=${appId}`;
+ebayApiUrl += '&GLOBAL-ID=EBAY-US';
+ebayApiUrl += '&RESPONSE-DATA-FORMAT=JSON';
+ebayApiUrl += '&paginationInput.entriesPerPage=1';
 
 app.post('/', bodyParser, (req, res) => {
   const { action, parameters } = req.body.result;
 
   switch (action) {
     case 'WEBHOOK_ACTION_PRODUCT':
-      getProductData(parameters.product)
-        .then(data => res.send(data));
+      getProductData(parameters.product).then(data => res.send(data));
+      break;
+    
+    case 'WEBHOOK_ACTION_PRODUCT_DEAL':
+      getProductDealsData(parameters.product).then(data => res.send(data));
       break;
 
-    case 'WEBHOOK_ACTION_CHUCK':
-      getChuckNorrisJoke()
-        .then(data => res.send(data));
+    case 'WEBHOOK_ACTION_JOKE':
+      getDadJoke().then(data => res.send(data));
       break;
     
     default:
@@ -28,27 +37,17 @@ app.post('/', bodyParser, (req, res) => {
 });
 
 function getProductData(product) {
-  let url = 'http://svcs.ebay.com/services/search/FindingService/v1'
-  url += '?OPERATION-NAME=findItemsByKeywords'
-  url += '&SERVICE-VERSION=1.0.0';
-  url += `&SECURITY-APPNAME=${appId}`;
-  url += '&GLOBAL-ID=EBAY-US';
-  url += '&RESPONSE-DATA-FORMAT=JSON';
-  url += '&callback=_cb_findItemsByKeywords';
-  url += '&REST-PAYLOAD';
-  url += `&keywords=${product}`;
-  url += '&paginationInput.entriesPerPage=3';
+  const url = `${ebayApiUrl}&keywords=${product}`;
 
   return superagent
     .get(url)
     .then(data => {
-      const length = data.text.length - 1;
-      const parsedData = data.text.substring(28, length);
-      const jsonObj = JSON.parse(parsedData)
-      
-      const selectedItem = jsonObj.findItemsByKeywordsResponse[0].searchResult[0].item[0];
-      let responseText = `I found this ${product} for you on eBay: "${selectedItem.title}".`
-      responseText += ` "${selectedItem.subtitle}."`;
+      const response = JSON.parse(data.text);
+      const selectedItem = response.findItemsByKeywordsResponse[0].searchResult[0].item[0];
+      console.log(selectedItem);
+
+      let responseText = `I found this "${product}" for you on eBay: ${selectedItem.title}`;
+      responseText += ` The current bid price is $${selectedItem.sellingStatus[0].currentPrice[0]['__value__']}.`;
       responseText += ` Here's the link: ${selectedItem.viewItemURL}`;
       
       console.log('responseText', responseText);
@@ -66,18 +65,47 @@ function getProductData(product) {
     })
 }
 
-function getChuckNorrisJoke() {
-  return superagent
-    .get('https://api.chucknorris.io/jokes/random')
-    .then(data => {
-      const jsonData = JSON.parse(data.text)
-      const responseText = jsonData.value;
-      console.log(responseText);
+function getProductDealsData(product) {
+  const url = `${ebayApiUrl}&keywords=${product}&ItemFilterType=BestOfferOnly=true`;
 
+  return superagent
+    .get(url)
+    .then(data => {
+      const response = JSON.parse(data.text);
+      const selectedItem = response.findItemsByKeywordsResponse[0].searchResult[0].item[0];
+      console.log(selectedItem);
+
+      let responseText = `I found this deal on "${product}" for you on eBay: ${selectedItem.title}`;
+      responseText += ` The current bid price is $${selectedItem.sellingStatus[0].currentPrice[0]['__value__']}.`;
+      responseText += ` Here's the link: ${selectedItem.viewItemURL}`;
+
+      console.log('responseText', responseText);
       return {
         speech: responseText,
         displayText: responseText,
         data: responseText,
+        contextOut: [],
+        source: ''
+      };
+    })
+    .catch(err => {
+      console.log(err);
+      return `I'm sorry, I couldn't find a deal on "${product}" for you on eBay.`;
+    })
+}
+
+function getDadJoke() {
+  return superagent
+    .get('https://icanhazdadjoke.com/')
+    .set('Accept', 'application/json')
+    .then(data => {
+      const { joke } = data.body;
+      console.log(joke);
+
+      return {
+        speech: joke,
+        displayText: joke,
+        data: joke,
         contextOut: [],
         source: ''
       };
